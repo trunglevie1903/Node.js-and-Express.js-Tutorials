494f49781dfbb92353895852be951d11201493f6
Trung Le Viet <trunglevie0319@gmail.com>
2022-05-09 19:59:18 +0700

    Update - Async Patterns

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-// npm - global command comes with node
-// local dependency - use only in this project
-// npm i <package name>
-// global dependency - use in any project
-// npm install -g <package name>
-// sudo npm install -g <package name> (macOS or ubuntu ?)
-// package.json - manifest file (stores important info about project/package)
-// manual approach 
-// npm init (step by step, press Enter to skip)
-// npm init -y (everything default)
+const { readFile } = require('fs');
 
-const _ = require('lodash');
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items);
-console.log(newItems);
-console.log('hello people');
+const getText = (path) => {
+    return new Promise((resolve, reject) => {
+        readFile(path, 'utf8', (err, data) => {
+            if (err) reject(err);
+            else {
+                resolve(data);
+            }
+        });
+    });
+};
+
+const start = async () => {
+    try {
+        const first = await getText('./content/first.txt');
+        const second = await getText('./content/second.txt');
+        console.log(first, second);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+start();
