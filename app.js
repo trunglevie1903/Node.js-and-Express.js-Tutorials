1cb113dfc13e61441f5e180925d332cc0c12c4c9
Trung Le Viet <trunglevie0319@gmail.com>
2022-05-09 20:13:07 +0700

    Update Async Patterns

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const { readFile, writeFile } = require('fs');
-const util = require('util');
-const readFilePromise = util.promisify(readFile);
-const writeFilePromise = util.promisify(writeFile);
+const { readFile, writeFile } = require('fs').promises;
+// const util = require('util');
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
 const start = async () => {
     try {
-        const first = await readFilePromise('./content/first.txt', 'utf8');
-        const second = await readFilePromise('./content/second.txt', 'utf8');
-        await writeFilePromise('./content/result-mind-grenade.txt', `This is Awful lol: ${first}, ${second}`);
+        const first = await readFile('./content/first.txt', 'utf8');
+        const second = await readFile('./content/second.txt', 'utf8');
+        await writeFile('./content/result-mind-grenade.txt', `This is Awful lol: ${first}, ${second}`, {flag: 'a'});
         console.log(first, second);
     } catch (err) {
         console.log(err);
