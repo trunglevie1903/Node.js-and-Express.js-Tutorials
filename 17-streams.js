60a98416e59587e234a9b5a4e33f9723ac820e28
Trung Le Viet <trunglevie0319@gmail.com>
2022-05-09 21:08:08 +0700

    Update linh tinh

---
+++ 17-streams.js
@@ -0,0 +1,11 @@
+const { createReadStream } = require('fs');
+const stream = createReadStream('./content/big.txt');
+// default 64 KB per chunk
+// highWaterMark - control chunk size, count as byte
+// const sstream = fs.createWriteStream('./content/big.txt', { encoding: 'utf8', highWaterMark: 90000 });
+stream.on('data', (result) => {
+    console.log(result);
+});
+stream.on('error', (err) => {
+    console.log(err);
+});
